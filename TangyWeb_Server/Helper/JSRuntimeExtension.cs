655ea3e1822a64970ff7ae116ed1a72460720140
Khashayar Nowruzi <khrnowruzi@gmail.com>
2023-06-26 10:05:34 +0330

    Adding ParentChild and Routing and JS components.

---
+++ TangyWeb_Server/Helper/JSRuntimeExtension.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.JSInterop;
+
+namespace TangyWeb_Server.Helper
+{
+    public static class JSRuntimeExtension
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+
+        public static async ValueTask ToastrError(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+
+        public static async ValueTask SweetAlertSuccess(this IJSRuntime jsRuntime,string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowSwal", "success", message);
+        }
+
+        public static async ValueTask SweetAlertFailure(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowSwal", "error", message);
+        }
+    }
+}
---
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,27 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, 'Operation Successful', { timeOut: 5000 })
+    }
+    else if (type === "error") {
+        toastr.error(message, 'Operation Failed', { timeOut: 5000 })
+    }
+}
+
+window.ShowSwal = (type, message) => {
+    if (type === "success") {
+        Swal.fire({
+            title: 'Success Notification!',
+            text: message,
+            icon: 'success',
+            confirmButtonText: 'Ok'
+        })
+    }
+    else if (type === "error") {
+        Swal.fire({
+            title: 'Error Notification!',
+            text: message,
+            icon: 'error',
+            confirmButtonText: 'Ok'
+        })
+    }
+}
