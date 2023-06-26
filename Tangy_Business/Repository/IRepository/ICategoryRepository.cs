784eda2b0fd0f319ad186b634446723f45c6c8ed
Khashayar Nowruzi <khrnowruzi@gmail.com>
2023-06-26 17:10:18 +0330

    Adding  ICategory Repository

---
+++ Tangy_Business/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Tangy_Models;
+
+namespace Tangy_Business.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public CategoryDTO Create(CategoryDTO obj);
+        public CategoryDTO Update(CategoryDTO obj);
+        public int Delete(int id);
+        public CategoryDTO Get(int id);
+        public IEnumerable<CategoryDTO> GetAll();
+    }
+}
