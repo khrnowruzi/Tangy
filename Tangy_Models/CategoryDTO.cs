7dcc3c628692225a1885fb3d16a39bf716c7646b
Khashayar Nowruzi <khrnowruzi@gmail.com>
2023-06-27 13:05:23 +0330

    Validations in Blazor Form

--- Tangy_Models/CategoryDTO.cs
+++ Tangy_Models/CategoryDTO.cs
@@ -1,8 +1,11 @@
-﻿namespace Tangy_Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tangy_Models
 {
     public class CategoryDTO
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Please enter a name..")]
         public string Name { get; set; }
     }
 }
