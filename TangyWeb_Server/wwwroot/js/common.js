d7baf12ce7cd848fac8a024e2b218a96f87d392b
Khashayar Nowruzi <khrnowruzi@gmail.com>
2023-06-27 16:44:52 +0330

    Delete Confirmation Component

--- TangyWeb_Server/wwwroot/js/common.js
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -25,3 +25,10 @@ window.ShowSwal = (type, message) => {
         })
     }
 }
+
+function ShowDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('show');
+}
+function HideDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('hide');
+}
