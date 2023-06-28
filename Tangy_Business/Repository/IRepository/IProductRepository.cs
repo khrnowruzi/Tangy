a6ac34b3964419e8acd34545747489caccd59b6b
Khashayar Nowruzi <khrnowruzi@gmail.com>
2023-06-28 12:12:48 +0330

    Adding IProduct Repository and Product Repository

---
+++ Tangy_Business/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Tangy_Models;
+
+namespace Tangy_Business.Repository.IRepository
+{
+    public interface IProductRepository
+    {
+        Task<ProductDTO> Create(ProductDTO obj);
+        Task<ProductDTO> Update(ProductDTO obj);
+        Task<int> Delete(int id);
+        Task<ProductDTO> Get(int id);
+        Task<IEnumerable<ProductDTO>> GetAll();
+    }
+}
