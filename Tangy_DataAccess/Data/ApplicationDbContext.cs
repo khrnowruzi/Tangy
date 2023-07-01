923387138559804dd0edf42633912a1f09850106
Khashayar Nowruzi <khrnowruzi@gmail.com>
2023-07-01 21:21:08 +0330

    Adding Product Price Model and DTO

--- Tangy_DataAccess/Data/ApplicationDbContext.cs
+++ Tangy_DataAccess/Data/ApplicationDbContext.cs
@@ -10,5 +10,6 @@ namespace Tangy_DataAccess.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet <Product> Products { get; set; }
+        public DbSet<ProductPrice> ProductPrices { get; set; }
     }
 }
