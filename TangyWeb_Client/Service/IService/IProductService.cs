fd96049ab7991ef06d022db6bfc87bac5945e450
Khashayar Nowruzi <khrnowruzi@gmail.com>
2023-07-04 12:34:45 +0330

    WASM-Adding Product Service Interface

---
+++ TangyWeb_Client/Service/IService/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using Tangy_Models;
+
+namespace TangyWeb_Client.Service.IService
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDTO>> GetAll();
+        Task<ProductDTO> Get(int productId);
+    }
+}
