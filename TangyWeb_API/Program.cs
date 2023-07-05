b5de0a36bc7b1c20d380ea771b0601cebe9a5d34
Khashayar Nowruzi <khrnowruzi@gmail.com>
2023-07-05 16:09:09 +0330

    WASM-Home Page UI, Retrieve Products from API

--- TangyWeb_API/Program.cs
+++ TangyWeb_API/Program.cs
@@ -15,6 +15,10 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddCors(o => o.AddPolicy("Tangy", builder =>
+{
+    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyMethod();
+}));
 
 var app = builder.Build();
 
@@ -26,6 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("Tangy");
 
 app.UseAuthorization();
 
